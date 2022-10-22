57aa5fdf520315bd9b7ce162afbbcdf1d6bc7a80
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-10-22 07:33:04 +0600

    Create an user with simple validation

--- src/components/RegisterReactBootstrup.js
+++ src/components/RegisterReactBootstrup.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../firebase/firebase.init';
+
+const auth = getAuth(app);
+
 
 const RegisterReactBootstrup = () => {
+
+  const [passwordError, setPasswordError] = useState();
 
     const handleRegister = event =>{
             event.preventDefault();
@@ -10,6 +17,28 @@ const RegisterReactBootstrup = () => {
             const email = event.target.email.value;
             const password = event.target.password.value;
             console.log(email, password);
+            createUserWithEmailAndPassword(auth, email, password)
+            .then(result =>{
+              const user = result.user;
+              console.log(user);
+            })
+            .catch(error =>{
+              console.error('error', error); 
+            })
+
+            if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+                setPasswordError('Please provide at list two uppercase'); 
+                return;
+            }
+            if(password.length < 6){
+                setPasswordError('please should be at list 6 character');
+                return;
+            }
+            if(!/(?=.*[!@#&&*])/.test(password)){
+                setPasswordError('Please add at list one special character'); 
+                return;
+            }
+            setPasswordError('');
     }
 
 
@@ -19,15 +48,15 @@ const RegisterReactBootstrup = () => {
 <Form onSubmit={handleRegister}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name='email' placeholder="Enter email" />
+        <Form.Control type="email" name='email' placeholder="Enter email" required/>
         
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name='password' placeholder="Password" />
+        <Form.Control type="password" name='password' placeholder="Password" required/>
       </Form.Group>
-      
+      <p className='text-danger'>{passwordError}</p>
       <Button variant="primary" type="submit">
         Register
       </Button>
